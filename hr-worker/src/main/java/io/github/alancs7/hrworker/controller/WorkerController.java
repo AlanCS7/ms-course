7501aa4fb78f84ab97cdf289b101ffda3ab157db
Alan Carlos <alanclash2541@gmail.com>
2022-12-27 21:56:34 -0300

    Ribbon load balancing

--- hr-worker/src/main/java/io/github/alancs7/hrworker/controller/WorkerController.java
+++ hr-worker/src/main/java/io/github/alancs7/hrworker/controller/WorkerController.java
@@ -3,7 +3,10 @@ package io.github.alancs7.hrworker.controller;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +19,11 @@ import io.github.alancs7.hrworker.services.WorkerService;
 @RestController
 @RequestMapping("/workers")
 public class WorkerController {
+	
+	private static Logger logger = LoggerFactory.getLogger(WorkerController.class);
+	
+	@Autowired
+	private Environment env;
 
 	@Autowired
 	private WorkerService service;
@@ -27,6 +35,8 @@ public class WorkerController {
 
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> getById(@PathVariable Long id) {
+		
+		logger.info("PORT = " + env.getProperty("local.server.port"));
 
 		Optional<Worker> worker = service.getById(id);
 		
