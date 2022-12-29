cf2e1d926f6a7c4f0d5cdc73d36acaefafcff6bd
Alan Carlos <alanclash2541@gmail.com>
2022-12-28 21:24:01 -0300

    Hystrix fault tolerance

--- hr-payroll/src/main/java/io/github/alancs7/hrpayroll/controller/PaymentController.java
+++ hr-payroll/src/main/java/io/github/alancs7/hrpayroll/controller/PaymentController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 import io.github.alancs7.hrpayroll.entities.Payment;
 import io.github.alancs7.hrpayroll.services.PaymentService;
 
@@ -17,9 +19,16 @@ public class PaymentController {
 	@Autowired
 	private PaymentService service;
 
+	@HystrixCommand(fallbackMethod = "getPaymentAlternative")
 	@GetMapping("/{workerId}/days/{days}")
 	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
 		Payment payment = service.getPayment(workerId, days);
 		return ResponseEntity.ok(payment);
 	}
+	
+	public ResponseEntity<Payment> getPaymentAlternative(Long workerId, Integer days) {
+		Payment payment = new Payment("Alan", 500.0, days);
+		return ResponseEntity.ok(payment);
+	}
+	
 }
