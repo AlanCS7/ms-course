4f3e87de1aa366fbefbf97cddb83fbc4a9ded0b4
Alan Carlos <alanclash2541@gmail.com>
2022-12-31 20:16:01 -0300

    hr-worker as a config server client

--- hr-worker/src/main/java/io/github/alancs7/hrworker/controller/WorkerController.java
+++ hr-worker/src/main/java/io/github/alancs7/hrworker/controller/WorkerController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.github.alancs7.hrworker.entities.Worker;
-import io.github.alancs7.hrworker.services.WorkerService;
+import io.github.alancs7.hrworker.services.WorkerService;	
 
 @RestController
 @RequestMapping("/workers")
@@ -22,12 +23,23 @@ public class WorkerController {
 	
 	private static Logger logger = LoggerFactory.getLogger(WorkerController.class);
 	
+	@Value("${test.config}")
+	private String testConfig;
+	
 	@Autowired
 	private Environment env;
 
 	@Autowired
 	private WorkerService service;
 
+
+	@GetMapping("/configs")
+	public ResponseEntity<Void> getConfigs() {
+		logger.info("CONFIG = " + testConfig);
+		return ResponseEntity.noContent().build();
+	}
+
+	
 	@GetMapping
 	public ResponseEntity<List<Worker>> getAll() {
 		return ResponseEntity.ok(service.getAll());
