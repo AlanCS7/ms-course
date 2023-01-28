ef4875de55ea4b3b9835b11053ccbb1c3a415f53
Alan Carlos <alanclash2541@gmail.com>
2023-01-28 18:31:19 -0300

    User, Role, many to many association

---
+++ hr-user/src/main/java/io/github/alancs7/hruser/entities/User.java
@@ -0,0 +1,47 @@
+package io.github.alancs7.hruser.entities;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "tb_user")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String name;
+	private String email;
+	private String password;
+
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(	name = "tb_user_role",
+				joinColumns = @JoinColumn(name = "user_id"), 
+				inverseJoinColumns = @JoinColumn(name = "role_id"))
+	@Setter(value = AccessLevel.NONE)
+	private final Set<Role> roles = new HashSet<>();
+	
+	
+}
